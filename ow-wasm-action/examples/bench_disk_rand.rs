cea3395d922e85857a01114f8c8e0950f0961da7
svbagga2 <svbagga2@sp26-cs525-1809.cs.illinois.edu>
2026-04-27 20:34:39 -0500

    Adding patches to wow to use on VMs

---
+++ ow-wasm-action/examples/bench_disk_rand.rs
@@ -0,0 +1,9 @@
+#[cfg(feature = "wasm")]
+ow_wasm_action::pass_json!(func);
+
+#[cfg(not(feature = "wasm"))]
+ow_wasm_action::json_args!(func);
+
+fn func(json: serde_json::Value) -> Result<serde_json::Value, anyhow::Error> {
+    bench_disk_rand::main(json).map_err(|e| anyhow::anyhow!("{e}"))
+}
